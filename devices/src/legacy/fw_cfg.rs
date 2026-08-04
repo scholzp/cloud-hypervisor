022eb00c207ce13bab43de8e5a085df013e85c39
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-08-04 16:37:12 +0200

    devices: Restrict x86 fw_cfg PIO reads to one byte
    In the traditional/POI interface the DATA register has a width of one
    byte on x86.[0]
    We therefore do not allow reads with higher widths. Currently, all
    tests assume a read with of 1 byte too.
    
    We reject `fw_cfg` in aarch64 builds for now as these changes introduce
    an incompatibility that adds to the incomplete implementation of it.
    Aarch64 support is a task to be solved in followups, as this also
    includes making corrections to the MMIO transport implementation and
    FDT corrections.
    
    [0] https://www.qemu.org/docs/master/specs/fw_cfg.html#data-register
    
    On-behalf-of: SAP pascal.scholz@sap.com
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>

--- devices/src/legacy/fw_cfg.rs
+++ devices/src/legacy/fw_cfg.rs
@@ -177,6 +177,10 @@ pub struct FwCfgItem {
     pub content: FwCfgContent,
 }
 
+#[cfg(all(feature = "fw_cfg", target_arch = "aarch64"))]
+compile_error!(
+    "fw_cfg is not supported on aarch64: the MMIO transport is incomplete and defective."
+);
 /// https://www.qemu.org/docs/master/specs/fw_cfg.html
 #[derive(Debug)]
 pub struct FwCfg {
@@ -776,7 +780,7 @@ impl BusDevice for FwCfg {
             (PORT_FW_CFG_SELECTOR, _) => {
                 error!("fw_cfg: selector register is write-only.");
             }
-            (PORT_FW_CFG_DATA, _) => _ = self.read_data(data, size as u32),
+            (PORT_FW_CFG_DATA, 1) => _ = self.read_data(data, size as u32),
             (PORT_FW_CFG_DMA_HI, 4) => {
                 let addr = self.dma_address;
                 let addr_hi = (addr >> 32) as u32;
